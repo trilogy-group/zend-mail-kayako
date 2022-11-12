0dad16adc27a5a72a4143eaef5fc14959fc93b04
DINH QUOC THANH <thanh.dinh@aurea.com>
2022-11-12 15:57:04 +0700

    bugfix\KAYAKOC-32013

--- src/Protocol/Pop3.php
+++ src/Protocol/Pop3.php
@@ -138,6 +138,28 @@ class Pop3
         }
     }
 
+	/**
+	 * Send requests
+	 *
+	 * @param string $requests your request in array
+	 * @throws Exception\RuntimeException
+	 */
+	public function sendRequests($requests = [])
+	{
+		ErrorHandler::start();
+		$result = '';
+		foreach ($requests as $request) {
+			if (fwrite($this->socket, $request . "\r\n") === false) {
+				throw new Exception\RuntimeException('cannot write - connection closed?');
+			}
+			$result = $result.fgets($this->socket)."\r\n";
+		}
+		$error  = ErrorHandler::stop();
+		if (! $result) {
+			throw new Exception\RuntimeException('send failed - connection closed?', 0, $error);
+		}
+		return $result;
+	}
     /**
      * read a response
      *
